4cb039c02cf0871ac03a9f73ccb890ae190b9c83
dell <piyush6348@gmail.com>
2016-12-26 18:06:51 +0530

    feat: widget completed

--- app/src/main/java/capstoneproject/androidnanodegree/com/capstoneproject2/activity/NewsDetailActivity.java
+++ app/src/main/java/capstoneproject/androidnanodegree/com/capstoneproject2/activity/NewsDetailActivity.java
@@ -1,29 +1,68 @@
 package capstoneproject.androidnanodegree.com.capstoneproject2.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.squareup.picasso.Picasso;
 
 import capstoneproject.androidnanodegree.com.capstoneproject2.R;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
+    private static final int REQUEST_INVITE = 100;
     private ImageView newsImage;
     private TextView newsTitle,newsDescription;
+    private Button shareNews;
+    public static String TAG;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_INVITE)
+        {
+            if (resultCode == RESULT_OK) {
+                // Get the invitation IDs of all sent messages
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                for (String id : ids) {
+                    Log.e(TAG, "onActivityResult: sent invitation " + id);
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
 
+        TAG=this.getClass().getSimpleName();
+
         newsImage=(ImageView) findViewById(R.id.news_image);
         newsTitle=(TextView) findViewById(R.id.news_heading);
         newsDescription=(TextView)findViewById(R.id.news_description);
+        shareNews=(Button)findViewById(R.id.share_news);
 
         Picasso.with(this).load(getIntent().getExtras().getString("url")).into(newsImage);
         newsTitle.setText(getIntent().getExtras().getString("title"));
         newsDescription.setText(getIntent().getExtras().getString("description"));
+
+        shareNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
+                        .setMessage(getString(R.string.invitation_message))
+                        .setCallToActionText(getString(R.string.invitation_cta))
+                        .build();
+                startActivityForResult(intent, REQUEST_INVITE);
+            }
+        });
     }
 }
---
+++ app/src/main/java/capstoneproject/androidnanodegree/com/capstoneproject2/widget/ItemWidgetList.java
@@ -0,0 +1,105 @@
+package capstoneproject.androidnanodegree.com.capstoneproject2.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import capstoneproject.androidnanodegree.com.capstoneproject2.R;
+import capstoneproject.androidnanodegree.com.capstoneproject2.database.DatabseColumns;
+import capstoneproject.androidnanodegree.com.capstoneproject2.database.QuoteProvider;
+
+
+public class ItemWidgetList extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new OtherWidgetList(this.getApplicationContext(), intent);
+    }
+}
+
+class OtherWidgetList implements RemoteViewsService.RemoteViewsFactory {
+    Cursor c;
+    private Context context = null;
+    private int appWidgetId;
+    RemoteViews row;
+
+    public OtherWidgetList(Context context, Intent intent) {
+
+        this.context = context;
+        appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    @Override
+    public void onCreate() {
+        Log.e("Item_Widget_Oncreate", "is running");
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        Log.e("Item_Widget_onDataSEt", "is running");
+        initData();
+    }
+
+    private void initData() {
+        if (c != null)
+            c.close();
+
+        c = context.getContentResolver().query(QuoteProvider.Quotes.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (c != null)
+            c.close();
+    }
+
+    @Override
+    public int getCount() {
+        return c.getCount();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+
+        Log.e("Item_Widget_getview", "is running");
+        String url = "";
+        String title = "";
+
+        if (c.moveToPosition(position)) {
+            url = c.getString(c.getColumnIndex(DatabseColumns.URL));
+
+            title = c.getString(c.getColumnIndex(DatabseColumns.TITLE));
+
+        }
+        Log.e("getViewAt: ", "hi " + title);
+        row = new RemoteViews(context.getPackageName(), R.layout.list_item_widget);
+        row.setTextViewText(R.id.heading, title);
+
+        return row;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
+
